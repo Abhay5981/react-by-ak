ab73f2ca1db1138e6a26995c94b7151c5d2e55a7
Abhay5981 <abhaykum78@gmail.com>
2024-09-04 18:05:54 +0530

    todoReducer toolkit

---
+++ 10todoReduxToolkit/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+ 
+
+  return (
+    <div>
+    
+        <h1>Redux ToolKit</h1>
+        <AddTodo/>
+        <Todos/>
+   
+        
+    </div>
+  )
+}
+
+export default App
