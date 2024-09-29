bbabb3d75a613bf261599e41f3a2cbc8c2ac795c
Abhay5981 <abhaykum78@gmail.com>
2024-09-29 13:11:55 +0530

    footer added

--- 11BlogProject/src/App.jsx
+++ 11BlogProject/src/App.jsx
@@ -1,15 +1,43 @@
-
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+
+import { Header, Footer } from './components'
+import {Outlet} from "react-router-dom"
+
 
 function App() {
- console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch()
 
 
-  return (
-    <>
-      <h1>A Blog app with Appwrite</h1> 
-    </>
-  )
+  useEffect(() =>{
+     authService.getCurrentUser()
+     .then((userData) => {
+       if (userData) {
+        dispatch(login({userData}))
+       }else{
+        dispatch(logout())
+       }
+     })
+     .finally(() => setloading(false))
+  }, [])
+ 
+ return !loading ? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+
+      <Header/>
+      <main>
+       TODO: <Outlet/>
+      </main>
+      <Footer/>
+    </div>
+  </div>
+ ) 
+ : null
 }
 
 export default App
